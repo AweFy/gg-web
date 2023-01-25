3b312cce691a75873ae0dcca0b8d504e9688457b
AweFy <101391275+AweFy@users.noreply.github.com>
2023-01-25 10:49:30 +0600

    Update Billing.jsx

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -18,9 +18,7 @@ const Billing = () => (
         invoicing
       </h2>
       <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
-        Elit enim sed massa etiam. Mauris eu adipiscing ultrices ametodio
-        aenean neque. Fusce ipsum orci rhoncus aliporttitor integer platea
-        placerat.
+        The perfect agency to help you to grow your website, business or any social media platform.
       </p>
 
       <div className="flex flex-row flex-wrap sm:mt-10 mt-6">
