72028c5f5cdaf7b7eb70b038a2a0eba2b1198cb1
AweFy <101391275+AweFy@users.noreply.github.com>
2023-01-25 11:00:24 +0600

    Update Business.jsx

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -22,13 +22,11 @@ const Business = () =>  (
   <section id="features" className={layout.section}>
     <div className={layout.sectionInfo}>
       <h2 className={styles.heading2}>
-        You do the business, <br className="sm:block hidden" /> we’ll handle
-        the money.
+        You do the business or content creation, <br className="sm:block hidden" /> we’ll handle
+        the Audience and Growth Strategy.
       </h2>
       <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
-        With the right credit card, you can improve your financial life by
-        building credit, earning rewards and saving money. But with hundreds
-        of credit cards on the market.
+        With the right Marketing Agency you can add a fabulous growth on your audience, marketing and money
       </p>
 
       <Button styles={`mt-10`} />
